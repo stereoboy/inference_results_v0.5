5231bb14a53e5a33346b32404b7339905061c54e
Jaeyoung Lee <jylee121@gmail.com>
2020-01-07 20:43:51 +0900

    update: shell script for harness
    Signed-off-by: Jaeyoung Lee <jylee121@gmail.com>

---
+++ closed/NVIDIA/run_harness.sh
@@ -0,0 +1,2 @@
+#make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream --test_mode=PerformanceOnly"
+make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream --test_mode=AccuracyOnly"
