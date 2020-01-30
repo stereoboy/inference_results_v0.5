26a4cfab4fb02e04d90a698ade85c396972d7166
Jaeyoung Lee <jylee0121@gmail.com>
2020-01-30 18:43:00 +0900

    update: setup for ssd-large GeforceRTX2080Ti
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ closed/NVIDIA/run_infer_large_geforcertx2080ti_int8_linear.sh
@@ -0,0 +1,1 @@
+python3 ./code/ssd-large/tensorrt/infer.py --engine_file ./build/engines/GeforceRTX2080Ti/ssd-large/SingleStream/ssd-large-SingleStream-gpu-b1-int8.plan --verbose --batch_size 1 --num_images 100
