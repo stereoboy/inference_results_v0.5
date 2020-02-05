ef1b0439c4b0e88ce4433906f585dbc240299931
Jaeyoung Lee <jylee0121@gmail.com>
2020-02-05 19:44:03 +0900

    update: add generated cache for ssd_mobilenet_v2_coco_2018_03_29
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- closed/NVIDIA/code/ssd-small/tensorrt/SSDMobileNet_for_ssd_mobilenet_v2_coco_2018_03_29.py
+++ closed/NVIDIA/code/ssd-small/tensorrt/SSDMobileNet_for_ssd_mobilenet_v2_coco_2018_03_29.py
@@ -133,13 +133,12 @@ class SSDMobileNet(BenchmarkBuilder):
         # Model path
         #self.model_path = dict_get(args, "model_path", default="build/models/SSDMobileNet/frozen_inference_graph.pb")
         self.model_path = dict_get(args, "model_path", default="build/models/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb")
-        #self.model_path = dict_get(args, "model_path", default="build/models/ssd_inception_v2_coco_2018_01_28/frozen_inference_graph.pb")
 
         if self.precision == "int8":
             calib_batch_size = dict_get(self.args, "calib_batch_size", default=1)
             calib_max_batches = dict_get(self.args, "calib_max_batches", default=500)
             force_calibration = dict_get(self.args, "force_calibration", default=False)
-            cache_file = dict_get(self.args, "cache_file", default="code/ssd-small/tensorrt/calibrator.cache")
+            cache_file = dict_get(self.args, "cache_file", default="code/ssd-small/tensorrt/calibrator_ssd_mobilenet_v2_coco_2018_03_29.cache")
             preprocessed_data_dir = dict_get(self.args, "preprocessed_data_dir", default="build/preprocessed_data")
             calib_data_map = dict_get(self.args, "calib_data_map", default="data_maps/coco/cal_map.txt")
             calib_image_dir = os.path.join(preprocessed_data_dir, "coco/train2017/SSDMobileNet/fp32")
