d5f5974278adc0a6d142deb1468e084f1eff4c76
Jaeyoung Lee <jylee0121@gmail.com>
2020-02-06 15:25:16 +0900

    update: add generated cache for ssd_inception_v2_coco_2018_01_28
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- closed/NVIDIA/code/ssd-small/tensorrt/SSDMobileNet_for_ssd_inception_v2_coco_2018_01_28.py
+++ closed/NVIDIA/code/ssd-small/tensorrt/SSDMobileNet_for_ssd_inception_v2_coco_2018_01_28.py
@@ -112,14 +112,13 @@ class SSDMobileNet(BenchmarkBuilder):
 
         # Model path
         #self.model_path = dict_get(args, "model_path", default="build/models/SSDMobileNet/frozen_inference_graph.pb")
-        #self.model_path = dict_get(args, "model_path", default="build/models/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb")
         self.model_path = dict_get(args, "model_path", default="build/models/ssd_inception_v2_coco_2018_01_28/frozen_inference_graph.pb")
 
         if self.precision == "int8":
             calib_batch_size = dict_get(self.args, "calib_batch_size", default=1)
             calib_max_batches = dict_get(self.args, "calib_max_batches", default=500)
             force_calibration = dict_get(self.args, "force_calibration", default=False)
-            cache_file = dict_get(self.args, "cache_file", default="code/ssd-small/tensorrt/calibrator.cache")
+            cache_file = dict_get(self.args, "cache_file", default="code/ssd-small/tensorrt/calibrator_ssd_inception_v2_coco_2018_01_28.cache")
             preprocessed_data_dir = dict_get(self.args, "preprocessed_data_dir", default="build/preprocessed_data")
             calib_data_map = dict_get(self.args, "calib_data_map", default="data_maps/coco/cal_map.txt")
             calib_image_dir = os.path.join(preprocessed_data_dir, "coco/train2017/SSDMobileNet/fp32")
@@ -134,7 +133,7 @@ class SSDMobileNet(BenchmarkBuilder):
         self.network = self.builder.create_network()
 
         # Do graph surgery on pb graph and convert to UFF.
-        uff_model = uff.from_tensorflow_frozen_model(self.model_path, preprocessor="code/ssd-small/tensorrt/SSDMobileNet.py")
+        uff_model = uff.from_tensorflow_frozen_model(self.model_path, preprocessor="code/ssd-small/tensorrt/SSDMobileNet_for_ssd_inception_v2_coco_2018_01_28.py")
 
         # Parse UFF model and populate network.
         parser = trt.UffParser()
