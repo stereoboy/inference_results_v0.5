afae6a8e0520f99282f672ea7ff7236b31ae6f3b
Jaeyoung Lee <jylee0121@gmail.com>
2020-06-04 19:17:58 +0900

    update: update inference scripts
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- closed/NVIDIA/run_infer_geforcertx2080ti_int8_chw4.sh
+++ closed/NVIDIA/run_infer_geforcertx2080ti_int8_chw4.sh
@@ -1,1 +1,4 @@
-python3 ./code/ssd-small/tensorrt/infer.py --engine_file ./build/engines/GeforceRTX2080Ti/ssd-small/SingleStream/ssd-small-SingleStream-gpu-b1-int8.plan --verbose --batch_size 1 --num_images 100
+python3 ./code/ssd-small/tensorrt/infer.py --engine_file ./build/engines/GeforceRTX2080Ti/ssd-small/SingleStream/ssd-small-SingleStream-gpu-b1-int8.plan --verbose --batch_size 1 --num_images 400
+python3 ./code/ssd-small/tensorrt/infer.py --engine_file ./build/engines/GeforceRTX2080Ti/ssd-small/SingleStreamB2/ssd-small-SingleStreamB2-gpu-b2-int8.plan --verbose --batch_size 2 --num_images 400
+python3 ./code/ssd-small/tensorrt/infer.py --engine_file ./build/engines/GeforceRTX2080Ti/ssd-small/SingleStreamB4/ssd-small-SingleStreamB4-gpu-b4-int8.plan --verbose --batch_size 4 --num_images 800
+python3 ./code/ssd-small/tensorrt/infer.py --engine_file ./build/engines/GeforceRTX2080Ti/ssd-small/SingleStreamB8/ssd-small-SingleStreamB8-gpu-b8-int8.plan --verbose --batch_size 8 --num_images 1600
