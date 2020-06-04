71a6c7c651cb7fea863a1abdaa7833482c40b299
Jaeyoung Lee <jylee0121@gmail.com>
2020-06-04 16:09:45 +0900

    new: script for calibration and engine generation
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ closed/NVIDIA/cal_gen.sh
@@ -0,0 +1,6 @@
+#make calibrate RUN_ARGS="--benchmarks=ssd-small"
+
+make generate_engines RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream"
+#make generate_engines RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB2"
+#make generate_engines RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB4"
+#make generate_engines RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB8"
