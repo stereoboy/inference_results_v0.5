bb4fa38fbc872074fa45d738f3f680b28635d6ca
Jaeyoung Lee <jylee121@gmail.com>
2020-06-22 13:17:12 +0900

    update: update scripts for test
    Signed-off-by: Jaeyoung Lee <jylee121@gmail.com>

--- closed/NVIDIA/run_harness.sh
+++ closed/NVIDIA/run_harness.sh
@@ -1,11 +1,10 @@
-DEVICE=GeforceRTX2080Ti
-#DEVICE=GeforceGTX1080Ti
-#DEVICE=Xavier
-#MODEL=ssd_mobilenet_v1_coco_2018_01_28
-MODEL=ssdlite_mobilenet_v2_coco_2018_05_09
-#MODEL=ssd_inception_v2_coco_2018_01_28
-#MODEL=ssdlite_mobilenet_v2_ball
-#MODEL=ssdlite_mobilenet_v1_ball_2020_06_04
+source ./settings.sh
+
+echo "##################################################################"
+echo "DEVICE=${DEVICE}"
+echo "MODEL=${MODEL}"
+echo "##################################################################"
+
 DATETIME=$(date '+%Y%m%d_%H%M%S');
 mkdir -p ./logs/${DEVICE}
 
@@ -13,6 +12,7 @@ mkdir -p ./logs/${DEVICE}
 #make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream --test_mode=PerformanceOnly --warmup_duration 20"
 
 start=`date +%s`
+cp -f ./build/engines/${DEVICE}/ssd-small/SingleStream/ssd-small-${MODEL}-SingleStreamB1-gpu-b1-int8.plan ./build/engines/${DEVICE}/ssd-small/SingleStream/ssd-small-SingleStream-gpu-b1-int8.plan 
 make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream --test_mode=PerformanceOnly --warmup_duration 5"       2>&1 | tee ./logs/${DEVICE}/${MODEL}_B1_${DATETIME}.log
 end=`date +%s`
 runtime=$((end-start))
@@ -20,6 +20,7 @@ echo "Elapsed Time: $runtime seconds"
 echo "Elapsed Time: $runtime seconds" >> ./logs/${DEVICE}/${MODEL}_B1_${DATETIME}.log
 
 start=`date +%s`
+cp -f ./build/engines/${DEVICE}/ssd-small/SingleStreamB2/ssd-small-${MODEL}-SingleStreamB2-gpu-b2-int8.plan ./build/engines/${DEVICE}/ssd-small/SingleStreamB2/ssd-small-SingleStreamB2-gpu-b2-int8.plan
 make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB2 --test_mode=PerformanceOnly --warmup_duration 5"     2>&1 | tee ./logs/${DEVICE}/${MODEL}_B2_${DATETIME}.log
 end=`date +%s`
 runtime=$((end-start))
@@ -27,6 +28,7 @@ echo "Elapsed Time: $runtime seconds"
 echo "Elapsed Time: $runtime seconds" >> ./logs/${DEVICE}/${MODEL}_B2_${DATETIME}.log
 
 start=`date +%s`
+cp -f ./build/engines/${DEVICE}/ssd-small/SingleStreamB4/ssd-small-${MODEL}-SingleStreamB4-gpu-b4-int8.plan ./build/engines/${DEVICE}/ssd-small/SingleStreamB4/ssd-small-SingleStreamB4-gpu-b4-int8.plan
 make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB4 --test_mode=PerformanceOnly --warmup_duration 5"     2>&1 | tee ./logs/${DEVICE}/${MODEL}_B4_${DATETIME}.log
 end=`date +%s`
 runtime=$((end-start))
@@ -34,6 +36,7 @@ echo "Elapsed Time: $runtime seconds"
 echo "Elapsed Time: $runtime seconds" >> ./logs/${DEVICE}/${MODEL}_B4_${DATETIME}.log
 
 start=`date +%s`
+cp -f ./build/engines/${DEVICE}/ssd-small/SingleStreamB8/ssd-small-${MODEL}-SingleStreamB8-gpu-b8-int8.plan ./build/engines/${DEVICE}/ssd-small/SingleStreamB8/ssd-small-SingleStreamB8-gpu-b8-int8.plan
 make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB8 --test_mode=PerformanceOnly --warmup_duration 5"     2>&1 | tee ./logs/${DEVICE}/${MODEL}_B8_${DATETIME}.log
 end=`date +%s`
 runtime=$((end-start))
@@ -41,5 +44,3 @@ echo "Elapsed Time: $runtime seconds"
 echo "Elapsed Time: $runtime seconds" >> ./logs/${DEVICE}/${MODEL}_B8_${DATETIME}.log
 
 make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream --test_mode=AccuracyOnly"                              2>&1 | tee ./logs/${DEVICE}/${MODEL}_AC_${DATETIME}.log
-#make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStreamB8 --test_mode=AccuracyOnly"
-#make run_harness RUN_ARGS="--benchmarks=ssd-small --scenarios=SingleStream"
---
+++ closed/NVIDIA/settings.sh
@@ -0,0 +1,9 @@
+DEVICE=GeforceRTX2080Ti
+#DEVICE=GeforceGTX1080Ti
+#DEVICE=Xavier
+MODEL=ssd_mobilenet_v1_coco_2018_01_28
+#MODEL=ssd_mobilenet_v2_coco_2018_03_29
+#MODEL=ssdlite_mobilenet_v2_coco_2018_05_09
+#MODEL=ssd_inception_v2_coco_2018_01_28
+#MODEL=ssdlite_mobilenet_v2_ball
+#MODEL=ssdlite_mobilenet_v1_ball_2020_06_04
